6f44f1cc827da84fbbacba4f37961fc071b50617
HemantNimje <nimje.hemant@gmail.com>
2016-11-29 10:46:19 -0800

    Add comments. Remove unwanted code.

--- src/ESTASolver.java
+++ src/ESTASolver.java
@@ -8,31 +8,28 @@ import java.util.PriorityQueue;
  * File created on 11/28/2016.
  */
 public class ESTASolver {
-    static int timeLimit = 6000;
-    static boolean goodWeather = true;
-    static boolean badWeather = false;
-    static boolean isChannel1Available = true;
-    static boolean isChannel2Available = true;
-    static boolean isChannel3Available = true;
-    static int lostMessagesCount = 0;
+    private static int timeLimit = 6000;
+    private static boolean goodWeather = true;
+    private static boolean badWeather = false;
+    private static boolean isChannel1Available = true;
+    private static boolean isChannel2Available = true;
+    private static boolean isChannel3Available = true;
+    private static int lostMessagesCount = 0;
 
     public static void main(String[] args) {
         ESTASolver solver = new ESTASolver();
 
-        UniformRealDistribution URD = new UniformRealDistribution();
-        double sample = URD.sample();
-        //System.out.println(sample);
-
         // Titles for the FEL
         System.out.println("EventName" + "\t" + "EventStatus" + "\t" + "EventTime");
 
-        // Create and insert Arrival events to the FEL
-        PriorityQueue<Event> eventPriorityQueue = new PriorityQueue<Event>();
-        Event eArrive = null;
+        // Create priority queue to maintain future event list
+        PriorityQueue<Event> eventPriorityQueue = new PriorityQueue<>();
         int eventOccurrenceTime = 0;
         int eventNumber = 1;
+
+        // Create and insert Arrival events to the FEL
         while (eventOccurrenceTime <= timeLimit) {
-            eArrive = new Event(eventNumber, "A", eventOccurrenceTime);
+            Event eArrive = new Event(eventNumber, "A", eventOccurrenceTime);
             eventNumber += 1;
             eventOccurrenceTime += 30;
             eventPriorityQueue.add(eArrive);
@@ -40,14 +37,19 @@ public class ESTASolver {
 
         // Create and insert the Good Weather and Bad Weather event
         int currentTime = 0;
+
+        /*
+        * Till the time reach limit of 100 hours i.e 6000 minutes and add the good weather and bad weather which will
+        * be required for further operations on priority queue
+        */
         while (currentTime < timeLimit) {
-            if (goodWeather == true) {
+            if (goodWeather) {
                 eventPriorityQueue.add(new Event(0, "GW", currentTime));
                 currentTime += (int) new NormalDistribution(90, 10).sample();
                 //eventPriorityQueue.add(new Event(0,"BW",currentTime));
                 goodWeather = false;
                 badWeather = true;
-            } else if (badWeather == true) {
+            } else if (badWeather) {
                 eventPriorityQueue.add(new Event(0, "BW", currentTime));
                 currentTime += ((int) new NormalDistribution(60, 20).sample());
                 //eventPriorityQueue.add(new Event(0,"GW",currentTime));
@@ -56,73 +58,107 @@ public class ESTASolver {
             }
         }
 
-        // Create and insert Leave events to the FEL
-        PriorityQueue<Event> FEL = new PriorityQueue<Event>();
-        Event eDepart = null;
+        // FEL - Future event list to keep track of the future events of arrival and departure
+        PriorityQueue<Event> FEL = new PriorityQueue<>();
         int departTime = 0;
         goodWeather = true;
         badWeather = false;
-        // Print the priority queue
+
+        // Create and insert Leave events along with the arrive events to the FEL
         while (!eventPriorityQueue.isEmpty()) {
 
-            if (eventPriorityQueue.peek().eventStatus == "GW") {
+            //pre-process the weather condition for further calculation of event processing time
+            if (eventPriorityQueue.peek().eventStatus.equals("GW")) {
                 //System.out.println("Good Weather");
                 eventPriorityQueue.poll();
                 goodWeather = true;
                 badWeather = false;
-            } else if (eventPriorityQueue.peek().eventStatus == "BW") {
+            } else if (eventPriorityQueue.peek().eventStatus.equals("BW")) {
                 //System.out.println("Bad Weather");
                 eventPriorityQueue.poll();
                 badWeather = true;
                 goodWeather = false;
             }
+
+            // get the top event from the priority queue
             Event currentEvent = eventPriorityQueue.poll();
-            //System.out.println(currentEvent.getEventNumber() + "\t" + currentEvent.getEventStatus() + "\t" + currentEvent.getTime());
 
-            if (currentEvent.eventStatus == "A") {
-                if (isChannel1Available == false && isChannel2Available == false && isChannel3Available == false) {
+            /*
+            * If the event is arrival then check whether any channel is available or not.
+            * If channel is available then calculate the processing time depending on the good weather or bad weather
+            * condition.
+            */
+            if (currentEvent.eventStatus.equals("A")) {
+                if (!isChannel1Available && !isChannel2Available && !isChannel3Available) {
                     lostMessagesCount += 1;
-                    System.out.println();
+                    System.out.println("Event " + currentEvent.eventNumber + " is lost");
                 } else {
-                    if (goodWeather == true) {
+                    if (goodWeather) {
                         departTime += (new UniformRealDistribution().sample()) * 60;
-                        eDepart = new Event(currentEvent.getEventNumber(), "D", departTime);
-                        //System.out.println(currentEvent.getEventNumber() + "\t" + currentEvent.getEventStatus() + "\t" + currentEvent.getTime());
-                        //System.out.println(eDepart.getEventNumber() + "\t" + eDepart.getEventStatus() + "\t" + eDepart.getTime());
+                        Event eDepart = new Event(currentEvent.getEventNumber(), "D", departTime);
+
+                        //print current event and its departure event
+                        /*System.out.println(currentEvent.getEventNumber() + "\t" + currentEvent.getEventStatus() + "\t"
+                                + currentEvent.getTime());
+                        System.out.println(eDepart.getEventNumber() + "\t" + eDepart.getEventStatus() + "\t"
+                                + eDepart.getTime());
+*/
                         eventPriorityQueue.add(eDepart);
                         FEL.add(currentEvent);
                         FEL.add(eDepart);
 
-                        if (isChannel1Available == true){ isChannel1Available = false; }
-                        else if (isChannel2Available == true){ isChannel2Available = false; }
-                        else if (isChannel3Available == true){ isChannel3Available = false; }
+                        if (isChannel1Available) {
+                            isChannel1Available = false;
+                        } else if (isChannel2Available) {
+                            isChannel2Available = false;
+                        } else if (isChannel3Available) {
+                            isChannel3Available = false;
+                        }
 
-                    } else if (badWeather == true) {
+                    } else if (badWeather) {
                         departTime += (Math.cbrt(new UniformRealDistribution().sample())) * 60;
-                        eDepart = new Event(currentEvent.getEventNumber(), "D", departTime);
-                        //System.out.println(currentEvent.getEventNumber() + "\t" + currentEvent.getEventStatus() + "\t" + currentEvent.getTime());
-                        //System.out.println(eDepart.getEventNumber() + "\t" + eDepart.getEventStatus() + "\t" + eDepart.getTime());
+                        Event eDepart = new Event(currentEvent.getEventNumber(), "D", departTime);
+
+                        //print current event and its departure event
+                       /* System.out.println(currentEvent.getEventNumber() + "\t" + currentEvent.getEventStatus() + "\t"
+                                + currentEvent.getTime());
+                        System.out.println(eDepart.getEventNumber() + "\t" + eDepart.getEventStatus() + "\t"
+                                + eDepart.getTime());
+*/
                         eventPriorityQueue.add(eDepart);
                         FEL.add(currentEvent);
                         FEL.add(eDepart);
 
-                        if (isChannel1Available == true){ isChannel1Available = false; }
-                        else if (isChannel2Available == true){ isChannel2Available = false; }
-                        else if (isChannel3Available == true){ isChannel3Available = false; }
+                        if (isChannel1Available) {
+                            isChannel1Available = false;
+                        } else if (isChannel2Available) {
+                            isChannel2Available = false;
+                        } else if (isChannel3Available) {
+                            isChannel3Available = false;
+                        }
                     }
                 }
             }
-            else if (currentEvent.eventStatus == "D"){
-                if (isChannel1Available == false){ isChannel1Available = true; }
-                else if (isChannel2Available == false){ isChannel2Available = true; }
-                else if (isChannel3Available == false){ isChannel3Available = true; }
+
+            // If currentEvent is departure event remove that event from the priority queue and release one channel
+            else if (currentEvent.eventStatus.equals("D")) {
+                if (!isChannel1Available) {
+                    isChannel1Available = true;
+                } else if (!isChannel2Available) {
+                    isChannel2Available = true;
+                } else if (!isChannel3Available) {
+                    isChannel3Available = true;
+                }
             }
 
-            while (!FEL.isEmpty()){
+            // Print the FEL
+            while (!FEL.isEmpty()) {
                 Event event = FEL.poll();
                 System.out.println(event.getEventNumber() + "\t" + event.getEventStatus() + "\t" + event.getTime());
             }
         }
+
+        // Print the number of messages that are lost
         System.out.println(lostMessagesCount);
     }
 }
